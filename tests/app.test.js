ba4a53b6dada17dcb1afbaea3808589ec95dde6b
SESA846601 <SESA846601@se.com>
2026-03-25 12:04:19 +0530

    added jest and test script

---
+++ tests/app.test.js
@@ -0,0 +1,8 @@
+describe('Backend Basic Tests', () => {
+  test('should pass basic math', () => {
+    expect(1 + 1).toBe(2);
+  });
+  test('should check string', () => {
+    expect('mern-app').toBeTruthy();
+  });
+});

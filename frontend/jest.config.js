70b45e9e1d660b7f586bc900aff0ba5a547df32d
SESA846601 <SESA846601@se.com>
2026-03-25 14:56:23 +0530

    changed test to just pass normal test

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,7 +1,3 @@
 module.exports={
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-  }
-};
+  testEnvironment: 'jsdom'
+  };
